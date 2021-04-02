86ac9f5f2386e962c56fa87eaae3607c855f25df
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-02 15:59:08 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,3 +20,13 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 let Counter = 0
 Counter = 0
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, y)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+}
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.clearScreen()
